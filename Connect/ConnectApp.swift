61274512f566de90cb4cda2f0535b1af13441024
Nihal-m24 <96053567+Nihal-m24@users.noreply.github.com>
2023-04-29 09:18:50 -0700

    Initial Commit

---
+++ Connect/ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectApp.swift
+//  Connect
+//
+//  Created by Nihal Memon on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
